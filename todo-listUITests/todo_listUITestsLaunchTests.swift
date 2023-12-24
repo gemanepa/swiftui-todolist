b1b9cec67e6c4899605585ee2c0aa370d9d6082d
gemanepa <contact@gemanepa.com>
2023-12-24 02:49:46 -0300

    Initial Commit

---
+++ todo-listUITests/todo_listUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  todo_listUITestsLaunchTests.swift
+//  todo-listUITests
+//
+//  Created by Gabriel Ernesto Martinez Canepa on 24/12/2023.
+//
+
+import XCTest
+
+final class todo_listUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
